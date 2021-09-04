2da4e43ae119afaed074435eeba3c5672e2c3212
Anna <bokarpro@gmail.com>
2021-09-04 17:51:43 +0300

    Edited task 1 and 2 from hometask4

--- hometask4/ht4_task1.py
+++ hometask4/ht4_task1.py
@@ -1,8 +1,7 @@
-my_list = [3, 6, 5, 0, 1]
-#my_list = list(range(6))
-sort_my_list = my_list
-sort_my_list = sorted(sort_my_list)
-if my_list == sort_my_list:
-    print('True: ', my_list, 'equal to', sort_my_list)
+user_input = input("Enter numbers: ")
+set_numbers = list(user_input)
+set_numbers_sort = sorted(set_numbers)
+if set_numbers == set_numbers_sort:
+    print('True: ', set_numbers, 'equal to', sorted(set_numbers_sort))
 else:
-    print('False: ', my_list, 'not equal to', sort_my_list)
+    print('False: ', set_numbers, 'not equal to', sorted(set_numbers_sort))
--- hometask4/ht4_task2.py
+++ hometask4/ht4_task2.py
@@ -1,6 +1,10 @@
-numbers = list(map(int, input('Enter the numbers set: ').split()))
-summa = 0
-for i in numbers:
-    if i % 2 == 0 and i > 0:
-        summa += i
-print(summa)
+numbers = input('Enter the numbers set: ')
+num_list = list()
+for item in numbers.split():
+    if item.isdigit():
+        num_list.append(item)
+result = 0
+for item in num_list:
+    result += int(item)
+print('Sum = ', result)
+
