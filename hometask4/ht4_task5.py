6716297da126b60bb9dd2f88a06c115eac0734bb
Anna <bokarpro@gmail.com>
2021-09-05 14:41:31 +0300

    5 task fixed

--- hometask4/ht4_task5.py
+++ hometask4/ht4_task5.py
@@ -2,9 +2,23 @@ hotels = {'Continental Hotel': '****',
           'Big Street Hotel': '**',
           'Corner Hotel': '**',
           'Trashviews Hotel': '*',
-          'Hazbins Hotel': '****',
+          'Hazbins Hotel': '***',
           'Hazbims Delux': '*****'}
 stars = input('Please enter stars: ')
-for key, value in hotels.items():
-    if len(value) == len(stars):
-        print(key, value)
+if stars.isdigit():
+    i = int(stars)
+    while i < 1 or i > 6:
+        print('Wrong stars number.')
+        stars = input('Enter form 1 to 5 range: ')
+        i = int(stars)
+    for key, value in hotels.items():
+        i_value = len(value)
+        if i == i_value:
+            print(key, value)
+else:
+    while len(stars) < 1 or len(stars) > 6:
+        print('Wrong stars number.')
+        stars = input('Enter form 1 to 5 range: ')
+    for key, value in hotels.items():
+        if len(stars) == len(value):
+            print(key, value)
--- main.py
+++ main.py
@@ -1,32 +1,4 @@
-import random
-symbols = input('Enter desired quantity symbols of the new password: ')
-symbols = int(symbols)
-while symbols < 8 or symbols > 16:
-    print('Wrong number of symbols. Correct entry is greater than 8 and less than 16')
-    symbols = input('Enter correct number of symbols: ')
-    symbols = int(symbols)
-numbers = '1234567890'
-numbers_list = list(numbers)
-random.shuffle(numbers_list)
-lower_letters = 'qwertyuioplkjhgfdsazxcvbnm'
-lower_letters_list = list(lower_letters)
-random.shuffle(lower_letters_list)
-upper_letters = lower_letters.upper()
-upper_letters_list = list(upper_letters)
-random.shuffle(upper_letters_list)
-i = 0
-password = list()
-while i < symbols:
-    while i < 2:
-        password.append(random.choice(numbers_list[i]))
-        i += 1
-    while i < 4:
-        password.append(random.choice(upper_letters_list[i]))
-        i += 1
-    password.append(random.choice(lower_letters_list[i]))
-    i += 1
-random.shuffle(password)
-print('Your new password is: ', ''.join(password))
-
-
+str1 = '***'
+i = len(str1)
+print(type(i), i)
 
